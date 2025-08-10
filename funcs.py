9e64f453abb4db55e07905ce87fb7e5d4ad54071
Caner Çakır <55649866+Canerss01@users.noreply.github.com>
2025-08-10 14:05:20 +0300

    all funcs added

--- funcs.py
+++ funcs.py
@@ -1,4 +1,3 @@
-import sqlite3
 import random
 import datetime
 
@@ -7,17 +6,23 @@ class StudentManagement():
         self.conn = con
         self.cursor = con.cursor()
     def addstudent(self, name, surname, bdate, number, major):
-        self.cursor.execute(f"insert into students values (?, ?, ?, ?, ?)", (name, surname, bdate, number, major))
+        self.cursor.execute("insert into students values (?, ?, ?, ?, ?)", (name, surname, bdate, number, major,))
         self.conn.commit()
 
-    def editstudent(self, number, name, newname):
-        pass
+    def editstudent(self, number, newname):
+        self.cursor.execute("Update students set name = ? where number = ?", (newname, number,))
+        self.conn.commit()
 
     def delstudent(self, number):
-        pass
+        self.cursor.execute("Delete from students where number = ?", (number,))
+        self.conn.commit()
 
     def liststudent(self):
-        pass
+        self.cursor.execute("Select * from students")
+        students = self.cursor.fetchall()
+        print("-----Öğrenciler-----")
+        for student in students:
+            print(f"Ad: {student[0]} {student[1]} | Bölüm: {student[4]} | Numara: {student[3]} | Doğum Tarihi: {student[2]}")
 
     def generate_student_number(self):
         year = datetime.datetime.now().year
@@ -33,21 +38,24 @@ class LessonManagement():
         self.conn = conn
         self.cursor = conn.cursor()
     def addlesson(self, name, credit, major):
-        self.cursor.execute("Select * from lessons Where name = ? and credit = ? and major = ?", (name, credit, major))
+        self.cursor.execute("Select * from lessons Where name = ? and credit = ? and major = ?", (name, credit, major,))
         if(self.cursor.fetchone()):
            print("Bu ders kaydı zaten bulunmakta.")
         else:
-           self.cursor.execute(f"insert into lessons values(?, ?, ?)",(name, credit, major))
+           self.cursor.execute("insert into lessons values(?, ?, ?)",(name, credit, major))
            self.conn.commit()
 
     def dellesson(self, name):
-
-
-         pass
+        self.cursor.execute("Delete from lessons where name = ?", (name))
+        self.conn.commit()
 
 
     def listlesson(self):
-        pass
+        self.cursor.execute("Select * from lessons")
+        print("-----Dersler-----")
+        for lesson in self.cursor.fetchall():
+            print(f"Ders adı: {lesson[0]} | Ders kredisi: {lesson[1]} | Dersin bölümü {lesson[2]}")
+
 
 
 
@@ -55,12 +63,26 @@ class RecManagement():
     def __init__(self, conn):
         self.conn = conn
         self.cursor = conn.cursor()
-    def givelesson(self, major, number, lesson):
-        pass
-
-    def listgivenlessons(self, number,):
-        pass
+    def givelesson(self, number, lesson, major):
+        self.cursor.execute("Insert into students_lessons values(?, ?, ?,)", (number, lesson, major,))
+        self.conn.commit()
+    def give_lesson_to_major(self, major, lesson):
+        self.cursor.execute("Select major = ? from students",(major))
+        major_students = self.cursor.fetchall()
+        for student in major_students:
+            self.cursor.execute("Insert into students_lessons values(?, ?, ?)", student[3], lesson, major,)
+    def listgivenlessons(self, number):
+        self.cursor.execute("Select * from students_lessons where number = ?", (number,))
+        student_lesson = self.cursor.fetchall()
+        self.cursor.execute("Select name, surname from students where number = ?", (number,))
+        student = self.cursor.fetchone()
+        if not student:
+            print("Öğrenci yok.")
+            return
+        print("Adı: " + student[0] + "Soyadı: " + student[1] + "Aldığı dersler:")
+        for lesson in student_lesson:
+            print(lesson[1])
 
 
     def delgivenlesson(self, number, lesson):
-        pass
+        self.cursor.execute("Delete from students_lessons Where number = ? and lesson = ?", (number, lesson,))
--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 import sqlite3
 def init_db():
     conn = sqlite3.connect("database.db")
+    conn.execute("PRAGMA foreign_keys = ON;")
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS students (name Text, surname Text, bdate Text, number TEXT UNIQUE, major Text)")
     cursor.execute("CREATE TABLE IF NOT EXISTS lessons (name Text, credit Integer, major Text)")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS students_lessons (
+    stu_number  TEXT NOT NULL,
+    stu_lessons TEXT NOT NULL,
+    stu_major Text Not Null,
+    PRIMARY KEY (stu_number, stu_lessons,stu_major),
+    FOREIGN KEY (stu_number)  REFERENCES students(number),
+    FOREIGN KEY (stu_lessons) REFERENCES lessons(name)
+    );""")
     conn.commit()
     conn.close()
